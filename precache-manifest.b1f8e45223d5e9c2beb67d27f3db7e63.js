f79fe4cdc7f01654f7a2476243a08492d1d953bb
Mario Vercellotti <mario@robocorp.com>
2020-05-12 16:42:46 +0300

    Updates

---
+++ precache-manifest.b1f8e45223d5e9c2beb67d27f3db7e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2334de1033a0781568331bebfbf0ed",
+    "url": "/rsb-intranet/index.html"
+  },
+  {
+    "revision": "edb82dbc07f7f8010c60",
+    "url": "/rsb-intranet/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e8e170db081714b2eedb",
+    "url": "/rsb-intranet/static/css/main.5b9e5941.chunk.css"
+  },
+  {
+    "revision": "edb82dbc07f7f8010c60",
+    "url": "/rsb-intranet/static/js/2.c7b3c681.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rsb-intranet/static/js/2.c7b3c681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e170db081714b2eedb",
+    "url": "/rsb-intranet/static/js/main.47330abc.chunk.js"
+  },
+  {
+    "revision": "c2ee83df18c86e0ac6dd",
+    "url": "/rsb-intranet/static/js/runtime-main.181e3a41.js"
+  }
+]);
