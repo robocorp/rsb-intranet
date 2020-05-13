57b92416b75b81e6a1fde274d1d86cdd7250f1e2
Jani Palsamäki <jani@robocorp.com>
2020-05-13 08:58:12 +0300

    Add gear emoji

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Logout from './Logout'
 const Header = () => (
   <header>
     <Logout />
-    <h1>RobotSpareBin</h1>
+    <h1>RobotSpareBin ⚙️</h1>
     <p>The leader in refurbished and dubious quality spare parts for robots!</p>
   </header>
 )
