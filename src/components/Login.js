05551b700f6c17a549f52e71341a009093f6dfad
Jani Palsamäki <jani@robocorp.com>
2020-05-13 07:27:03 +0300

    Update login layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,24 +11,36 @@ function Login() {
   }
 
   return (
-    <form onSubmit={event => handleSubmit(event, login)}>
-      <div className="form-group">
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" required className="form-control" />
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          <form onSubmit={event => handleSubmit(event, login)}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                required
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                required
+                className="form-control"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Log in
+            </button>
+          </form>
+        </div>
+        <div className="col-sm"></div>
       </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          required
-          className="form-control"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Log in
-      </button>
-    </form>
+    </div>
   )
 }
 
