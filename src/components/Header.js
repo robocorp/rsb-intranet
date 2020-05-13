d6ccf81448348fb3547888201a15d9321e80ec37
Jani Palsamäki <jani@robocorp.com>
2020-05-13 07:32:03 +0300

    Update header visuals

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Logout from './Logout'
 
 const Header = () => (
-  <>
+  <header>
     <Logout />
     <h1>RobotSpareBin</h1>
-  </>
+    <p>The leader in refurbished and dubious quality spare parts for robots!</p>
+  </header>
 )
 
 export default Header
