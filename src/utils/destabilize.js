249eaeccd62c724c7f3f8d7e10acb4b19f0bf0a6
Jani Palsamäki <jani@robocorp.com>
2020-05-14 08:38:16 +0300

    Destabilize: Decrease max delay, error out less often (1 in 50)

--- src/utils/destabilize.js
+++ src/utils/destabilize.js
@@ -1,11 +1,11 @@
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 
 const destabilize = () => {
-  if (getRandomInt(10) === 7) {
+  if (getRandomInt(50) === 7) {
     throw new Error('Have you tried turning it off and on again?')
   }
 
-  return new Promise(resolve => setTimeout(resolve, getRandomInt(5000)))
+  return new Promise(resolve => setTimeout(resolve, getRandomInt(3000)))
 }
 
 export default destabilize
