d68ff86103fbbed96a5377f402e1e8a24099ac23
Mario Vercellotti <mario@robocorp.com>
2020-05-14 12:11:46 +0300

    Add explanation and some styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,12 +34,41 @@ function Login() {
                 className="form-control"
               />
             </div>
+            <small id="passwordHelpBlock" class="form-text text-muted">
+              Psst... you can login using "maria" as username and
+              "thoushallnotpass" as password. But don't tell anyone!
+            </small>
             <button type="submit" className="btn btn-primary">
               Log in
             </button>
           </form>
         </div>
-        <div className="col-sm"></div>
+        <div className="col-sm">
+          <div className="alert  alert-info" role="alert">
+            <h4>Hey! Wait a minute... What is this site?</h4>
+            <hr></hr>
+            <p>
+              This is a demonstration site built by {}
+              <a href="https://www.robocorp.com">Robocorp</a>, and we use it for
+              demos and our learning courses... Think about it as a gym for
+              Software Robots!
+            </p>
+
+            <p>
+              You are absolutely free to play around with it. Mind you,
+              sometimes it's intentionally a bit buggy!
+              <span role="img" aria-label="sweating smily face emoticon">
+                😅
+              </span>
+            </p>
+
+            <p>
+              If this sounds interesting,{' '}
+              <a href="https://hub.robocorp.com">Visit us</a> to learn how to
+              make your own Software Robots!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
