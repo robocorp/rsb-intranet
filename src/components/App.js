b2b22a24268b8a77f5b8cc3bde1ad47f598c2f8c
Jani Palsamäki <jani@robocorp.com>
2020-05-18 15:52:58 +0300

    Make destabilization optional, control by URL param

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,23 @@ import Intranet from './Intranet'
 import Login from './Login'
 import './App.css'
 
+const rsbDestabilizeKey = 'rsbDestabilize'
+
+const setDestabilize = () => {
+  const destabilizeParam = new URLSearchParams(window.location.search).get(
+    'destabilize',
+  )
+
+  if (destabilizeParam === 'true') {
+    window.localStorage.setItem(rsbDestabilizeKey, true)
+  } else if (destabilizeParam === 'false') {
+    window.localStorage.removeItem(rsbDestabilizeKey)
+  }
+}
+
 function Home() {
   const {user} = useAuthState()
+  setDestabilize()
   return user ? <Intranet /> : <Login />
 }
 
--- src/utils/destabilize.js
+++ src/utils/destabilize.js
@@ -1,7 +1,9 @@
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 
+const shouldDestabilize = () => window.localStorage.getItem('rsbDestabilize')
+
 const destabilize = () => {
-  if (getRandomInt(50) === 7) {
+  if (shouldDestabilize() && getRandomInt(10) === 7) {
     throw new Error('Have you tried turning it off and on again?')
   }
 
