2ae12120b2be5bdd4186306dd449e4d8fad1ae7b
Mario Vercellotti <mario@robocorp.com>
2020-05-28 15:31:50 +0300

    Fixes for the footer getting in the way

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <footer className="fixed-bottom">
-      Copyright &copy; 2020 Robocorp Technologies, Inc.
-    </footer>
-  )
+  return <footer>Copyright &copy; 2020 Robocorp Technologies, Inc.</footer>
 }
 
 export default Footer
