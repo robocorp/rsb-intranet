333e226ef448903c1b935f6a9f0aa1179e38739f
Rihards Gravis <rihards@roboocorp.com>
2020-09-30 10:19:04 +0300

    Hide menu when user not authorized

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Link, useLocation} from 'react-router-dom'
+import {useAuthState} from '../contexts/auth'
 import Logout from './Logout'
 
 const Header = () => {
+  const {user} = useAuthState()
   const {pathname} = useLocation()
 
   return (
-    <header className="header-container">
+    <header className={`header-container${user ? ' auth' : ''}`}>
       <div className="container">
         <Logout />
         <h1>
@@ -22,24 +24,28 @@ const Header = () => {
           The leader in refurbished and dubious quality spare parts for robots,
           since 1982!
         </h5>
-        <ul className="nav nav-pills">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={`nav-link${pathname === '/' ? ' active' : ''}`}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/feedback"
-              className={`nav-link${pathname === '/feedback' ? ' active' : ''}`}
-            >
-              Feedback
-            </Link>
-          </li>
-        </ul>
+        {user && (
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={`nav-link${pathname === '/' ? ' active' : ''}`}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/feedback"
+                className={`nav-link${
+                  pathname === '/feedback' ? ' active' : ''
+                }`}
+              >
+                Feedback
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </header>
   )
