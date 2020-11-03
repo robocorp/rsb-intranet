193292983d7a9e5231248fe632ea9a8af5aeaca7
Mario Vercellotti <mario@robocorp.com>
2020-11-03 15:25:37 +0200

    Add static robot preview mockup

--- src/components/robot-order/RobotOrder.js
+++ src/components/robot-order/RobotOrder.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import RobotOrderForm from './RobotOrderForm'
+import RobotPreview from './RobotPreview'
 
 function RobotOrder() {
   return (
     <>
-      <h2>Build your robot!</h2>
-      <p>
-        Everyone needs a robot, but all needs are unique. Build your own robot
-        and we ship it to you as easy-to-assemble package, delivered straight to
-        your front door!
-      </p>
-      <RobotOrderForm />
+      <div className="container">
+        <h2>Build your robot!</h2>
+        <p>
+          Everyone needs a robot, but all needs are unique. Build your own robot
+          and we ship it to you as easy-to-assemble package, delivered straight
+          to your front door!
+        </p>
+        <div className="row">
+          <div className="col-sm">
+            <RobotOrderForm />
+          </div>
+          <div className="col-sm">
+            <RobotPreview />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
